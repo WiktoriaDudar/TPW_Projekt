385cde6e8eb53d5eeb43e35f89f6e46787aba9b9
Wiktoria Dudar <254741@edu.p.lodz.pl>
2026-03-13 19:34:00 +0100

    nowe klasy etap 0

---
+++ Tests/Test1.cs
@@ -0,0 +1,12 @@
+﻿namespace Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
